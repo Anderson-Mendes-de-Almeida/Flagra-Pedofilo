c29a84d11f321afd31fe6577c0f1fc566bb7e5f9
Anderson Mendes <anderson.mda@outlook.com.br>
2024-06-27 19:06:14 -0300

    primeiro commit

---
+++ FlagraPedofilo/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informações gerais sobre um assembly são controladas através do seguinte 
+' conjunto de atributos. Altere o valor destes atributos para modificar a informação
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("FlagraPedofilo")> 
+<Assembly: AssemblyDescription("SoftWare de segurança")> 
+<Assembly: AssemblyCompany("SkyNetSystem")> 
+<Assembly: AssemblyProduct("FlagraPedofilo")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("SkyNetSystem")> 
+
+<Assembly: ComVisible(False)> 
+
+'O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
+<Assembly: Guid("fce14bae-b393-4983-a0e8-32a727bdb132")> 
+
+' Informação de versão para um assembly consiste nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
+' utilizando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("pt-BR")> 
